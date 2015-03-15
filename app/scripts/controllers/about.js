67fd33e199ab6ad7fe418a26af22e861aa3e2ff0
Jack <jackov@126.com>
2015-03-15 23:53:55 +0800

    一周就这些?

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name dfinderApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the dfinderApp
+ */
+angular.module('dfinderApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name dfinderApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the dfinderApp
+ */
+angular.module('dfinderApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
